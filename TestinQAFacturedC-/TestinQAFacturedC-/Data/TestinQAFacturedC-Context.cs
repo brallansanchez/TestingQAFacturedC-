f184988ec4d8ca82c7f4f0873bdabce6d3651cd5
brallansanchez <brsr1234@gmail.com>
2023-09-07 15:30:49 -0600

    DbContext

---
+++ TestinQAFacturedC-/TestinQAFacturedC-/Data/TestinQAFacturedC-Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TestinQAFacturedC_.Data
+{
+    public class TestinQAFacturedC_Context:DbContext
+    {
+        public TestinQAFacturedC_Context(DbContextOptions options):base(options)
+        {
+
+        }
+       
+    }
+}
